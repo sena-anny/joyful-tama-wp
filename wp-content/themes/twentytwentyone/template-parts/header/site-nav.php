a65cfcd8014dee1da7c4a4154272a30d8416147c
Koji Nakamura <cozykoji18@gmail.com>
2021-07-31 20:08:01 +0000

    fix: site-nav delete menu literal

--- wp-content/themes/twentytwentyone/template-parts/header/site-nav.php
+++ wp-content/themes/twentytwentyone/template-parts/header/site-nav.php
@@ -13,7 +13,7 @@
 	<nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
 		<div class="menu-button-container">
 			<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
-				<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
+				<span class="dropdown-icon open">
 					<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
 				</span>
 				<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'twentytwentyone' ); ?>
